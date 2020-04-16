9d34604013cc30932b81959f6f82df7600846a0f
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-04-16 18:34:50 +0545

    updated

--- app.js
+++ app.js
@@ -8,16 +8,54 @@ var budgetController = (function(){
 
 //UI CONTROLLER
 var UIController=(function(){
+    var DOMstrings={
+        inputType: '.add__type' , //storing strings
+        inputDescription:'.add__description',
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
+    };
   
+    return{                //returning all inputs of UI
+      getinput:function(){
+          return{
+
+            type :document.querySelector(DOMstrings.inputValue).value, //will be either inc(+) or exp(-)
+            description : document.querySelector(DOMstrings.inputDescription).value,
+            value : document.querySelector(DOMstrings.inputValue).value
+          }
+        },
+        getDOMStrings:function(){
+            return DOMstrings;
+        }
+  }
 
 })();
 
 //GLOBAL APP CONTROLERS
 var controller=(function(budgetCtrl,UICtrl){
-   
+
+    var setupEventListners=function(){
+        
+        var DOM=UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+        document.addEventListener('keypress',function(event){
+       
+            if(event.keyCode===13 || event.which === 13){  //this means execute only when enter key is pressed 
+            ctrlAddItem();
+             }
+
+        });
+
+    };
+    
+    
+
     var ctrlAddItem = function(){
 
-         //1.Get the field input data
+       //1.Get the field input data
+       var input = UICtrl.getinput();
 
        //2.Add item to the budget controller
        
@@ -26,16 +64,15 @@ var controller=(function(budgetCtrl,UICtrl){
        //4.Calculate the budget
 
        //5.Display the budget on the UI
-       console.log('It works');
-    }
+       
+    };
    
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
-
-   document.addEventListener('keypress',function(event){
-       if(event.keyCode===13 || event.which === 13){  //this means execute only when enter key is pressed 
-       ctrlAddItem();
-       }
-   });
-
+    return{
+        init:function(){
+           console.log('App started');
+           setupEventListners();
+        }
+    }
 })(budgetController,UIController);
 
+controller.init();
